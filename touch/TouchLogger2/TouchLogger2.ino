8f78ede0cb16aac6c10b3e851c1532c5c62fc175
Michael Hoser <michael.hoser@outlook.com>
2024-02-06 21:26:38 +0100

    Class based version of touch logger

---
+++ touch/TouchLogger2/TouchLogger2.ino
@@ -0,0 +1,49 @@
+// ----------------------------------------------------------------------------
+// Elecrow HMI 7 display - Touch Logger Sample (V2)
+// ----------------------------------------------------------------------------
+// Board: ESP32S3 Dev Module
+// Port: COM3 (or the one related to CH340G in device maganer)
+// ----------------------------------------------------------------------------
+// Tools > Partition Scheme: Huge APP (3MB No OTA/1MB SPIFFS)
+// Tools > PSRAM: OPI PSRAM
+// ----------------------------------------------------------------------------
+// Required Libs:
+// - TAMC_GT911, by TAMC, v1.02
+// ----------------------------------------------------------------------------
+// To upload, press and hold BOOT while pressing RESET to enter Firmware mode.
+// ----------------------------------------------------------------------------
+
+#include "Touchpad.hpp"
+
+Touchpad tp;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println("Touch Logger Sample V2");
+  Serial.println("Please touch the screen to see the coordinates.");
+
+  tp.init();
+}
+
+void loop()
+{
+  delay(10);
+
+  switch (tp.update())
+  {
+  case Touchpad::UpdateResult::Pressed:
+    Serial.print("PRESSED");
+    break;
+  case Touchpad::UpdateResult::Released:
+    Serial.print("RELEASED");
+    break;
+  case Touchpad::UpdateResult::Reset:
+    Serial.print("RESET");
+    break;
+  }
+
+  Serial.printf(" (%d:%d) - %d", tp.x(), tp.y(), tp.active());
+  Serial.println();
+}
